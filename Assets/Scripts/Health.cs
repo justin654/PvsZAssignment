abb28982c5eba4663d9638bd325cba694e26e98f
justin654 <justin654@gmail.com>
2023-11-30 23:04:19 -0500

    Health
    Basic health script, pretty much same script used in my side zombie project

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float health = 100f;
+    [SerializeField] private GameObject deathEffectPrefab;
+    [SerializeField] private float deathEffectDuration = 2f;
+    
+    public void Damage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+    
+    private void Death()
+    {
+        if (deathEffectPrefab != null)
+        {
+            GameObject effect = Instantiate(deathEffectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, deathEffectDuration);
+        }
+
+        Destroy(gameObject); // destroy go
+    }
+}
