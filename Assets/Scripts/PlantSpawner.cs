69d43a827e65e5d50abfada0a15ebb0b9411ecc1
justin654 <justin654@gmail.com>
2023-12-01 04:24:20 -0500

    Offset for Plant
    Offset setup to get the skeleton archers right, i'm sure there is a much better way to do this without the messyness of offsets but

--- Assets/Scripts/PlantSpawner.cs
+++ Assets/Scripts/PlantSpawner.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlantSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject plantPrefab;
+    [SerializeField] private Vector3 spawnOffset = Vector3.zero; // offset spawn position to get them lined up
 
-    public void SpawnPlayer(Vector3 position_To_Spawn)
+    public void SpawnPlayer(Vector3 positionToSpawn)
     {
-        Instantiate(plantPrefab, position_To_Spawn, Quaternion.identity);
+        Vector3 finalPosition = positionToSpawn + spawnOffset; // apply offset
+        Instantiate(plantPrefab, finalPosition, Quaternion.identity);
     }
 }
