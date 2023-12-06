cdfafb2b909b550e50e60a6d6ca308eb4ac786af
justin654 <justin654@gmail.com>
2023-12-05 19:21:44 -0500

    Lose
    Set up the lose scene and logic, added in some audio

--- Assets/Scripts/Lose.cs
+++ Assets/Scripts/Lose.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Lose : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private string loseSceneName = "GameOver";
+    [SerializeField] private AudioClip eatingBrainsSound;
+
+    private AudioSource audioSource;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = eatingBrainsSound;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Enemies"))
+        {
+            PlayEatingBrainsSound();
+            StartCoroutine(WaitAndLoadScene());
+        }
+    }
+
+    private void PlayEatingBrainsSound()
+    {
+        audioSource.Play();
+    }
+
+    private IEnumerator WaitAndLoadScene()
+    {
+        yield return
+            new WaitForSeconds(audioSource.clip.length); // we wait for the clip length before loading next scene
+        SceneManager.LoadScene(loseSceneName);
     }
 }
